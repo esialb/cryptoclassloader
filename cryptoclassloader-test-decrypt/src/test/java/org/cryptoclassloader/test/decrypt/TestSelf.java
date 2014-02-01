d3f284abebcf28307ef8d2177343be298544f60d
robin <robin.kirkman@gmail.com>
2014-02-01 01:33:51 -0500

    Test that the class was actually encrypted

--- cryptoclassloader-test-decrypt/src/test/java/org/cryptoclassloader/test/decrypt/TestSelf.java
+++ cryptoclassloader-test-decrypt/src/test/java/org/cryptoclassloader/test/decrypt/TestSelf.java
@@ -2,6 +2,8 @@ package org.cryptoclassloader.test.decrypt;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Arrays;
 
 import org.cryptoclassloader.CryptoClassLoader;
@@ -13,12 +15,24 @@ public class TestSelf {
 	public void testSelf() throws Exception {
 		byte[] key = "foobar".getBytes("UTF-8");
 		key = Arrays.copyOf(key, 16);
-		CryptoClassLoader cl = new CryptoClassLoader(key, new File("target/dependency/cryptoclassloader-test-encrypt.jar").getCanonicalFile().toURL());
+		ClassLoader cl = new CryptoClassLoader(key, new File("target/dependency/cryptoclassloader-test-encrypt.jar").getCanonicalFile().toURL());
 		
 		Class<?> Self = cl.loadClass("org.cryptoclassloader.test.encrypt.Self");
 		Method Self_self = Self.getMethod("self", Object.class);
 		
 		Assert.assertEquals("foo", Self_self.invoke(null, "foo"));
 		
+	}
+	
+	@Test
+	public void testSelfWithoutDecryption() throws Exception {
+		ClassLoader cl = new URLClassLoader(new URL[] {new File("target/dependency/cryptoclassloader-test-encrypt.jar").getCanonicalFile().toURL()});
+
+		try {
+			Class<?> Self = cl.loadClass("org.cryptoclassloader.test.encrypt.Self");
+			Assert.fail("class wasn't encrypted");
+		} catch(ClassFormatError e) {
+			// expected
+		}
 	}
 }
