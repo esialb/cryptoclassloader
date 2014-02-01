157ff207ca2901b3b90a0a88216f816ee1238053
robin <robin.kirkman@gmail.com>
2014-02-01 00:22:08 -0500

    Class to convert a zip file to a cryuptozip

---
+++ cryptoclassloader/src/main/java/org/cryptoclassloader/CryptoZipConverter.java
@@ -0,0 +1,49 @@
+package org.cryptoclassloader;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+public class CryptoZipConverter {
+	protected static class NoCloseOutputStream extends FilterOutputStream {
+		public NoCloseOutputStream(OutputStream out) {
+			super(out);
+		}
+
+		@Override
+		public void close() throws IOException {
+		}
+	}
+	
+	protected byte[] key;
+	
+	public CryptoZipConverter(byte[] key) {
+		this.key = key;
+	}
+	
+	public void convert(ZipFile zip, OutputStream out) throws IOException, GeneralSecurityException {
+		ZipOutputStream zout = new ZipOutputStream(out);
+		Enumeration<? extends ZipEntry> zee = zip.entries();
+		while(zee.hasMoreElements()) {
+			ZipEntry ze = zee.nextElement();
+			InputStream in = zip.getInputStream(ze);
+			zout.putNextEntry(ze);
+			OutputStream eout = new NoCloseOutputStream(zout);
+			eout = new AESOutputStream(eout, key);
+			
+			byte[] buf = new byte[1024];
+			for(int r = in.read(buf); r != -1; r = in.read(buf))
+				eout.write(buf, 0, r);
+			
+			eout.close();
+			zout.closeEntry();
+		}
+		zout.finish();
+	}
+}
