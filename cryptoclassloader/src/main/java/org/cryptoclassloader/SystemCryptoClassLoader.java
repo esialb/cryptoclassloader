85a8acf9e8f4343d26db3fcaf89f97842b22a48d
robin <robin.kirkman@gmail.com>
2014-02-01 02:48:08 -0500

    Revamp SystemCryptoClassLoader

--- cryptoclassloader/src/main/java/org/cryptoclassloader/SystemCryptoClassLoader.java
+++ cryptoclassloader/src/main/java/org/cryptoclassloader/SystemCryptoClassLoader.java
@@ -1,22 +1,28 @@
 package org.cryptoclassloader;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.GeneralSecurityException;
 import java.util.Arrays;
 
-public class SystemCryptoClassLoader extends CryptoClassLoader {
+public class SystemCryptoClassLoader extends URLClassLoader {
 	private static byte[] propertyKey() throws UnsupportedEncodingException {
 		byte[] key = System.getProperty("cryptoclassloader.key").getBytes("UTF-8");
 		return Arrays.copyOf(key, 16);
 	}
 	
+	protected byte[] key;
 	protected ClassLoader bootstrap;
 	
 	public SystemCryptoClassLoader(ClassLoader bootstrap) throws UnsupportedEncodingException {
-		super(bootstrap, propertyKey());
+		super(new URL[0], bootstrap);
 		this.bootstrap = bootstrap;
+		this.key = propertyKey();
 	}
 	
 	@Override
@@ -35,6 +41,46 @@ public class SystemCryptoClassLoader extends CryptoClassLoader {
 			return super.loadClass(name);
 		} catch (MalformedURLException e) {
 			return bootstrap.loadClass(name);
+		}
+	}
+
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		String resource = name.replaceAll("\\.", "/") + ".class";
+		URL url = getResource(resource);
+		try {
+			InputStream in = url.openStream();
+			try {
+				in = new AESInputStream(in, key);
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+				byte[] buf = new byte[1024];
+				for(int r = in.read(buf); r != -1; r = in.read(buf))
+					out.write(buf, 0, r);
+
+				buf = out.toByteArray();
+				return defineClass(name, buf, 0, buf.length);
+			} finally {
+				in.close();
+			}
+		} catch (IOException e) {
+			throw new ClassNotFoundException(name, e);
+		} catch (GeneralSecurityException e) {
+			throw new ClassNotFoundException(name, e);
+		}
+	}
+	
+	@Override
+	public InputStream getResourceAsStream(String name) {
+		try {
+			InputStream in = super.getResourceAsStream(name);
+			if(in == null)
+				return null;
+			return new AESInputStream(in, key);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (GeneralSecurityException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
